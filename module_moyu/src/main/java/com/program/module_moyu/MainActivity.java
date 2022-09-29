73004d1480f90ec1d77c56980ad102b5f3d50a30
precontext <815172923@qq.com>
2022-09-29 20:17:57 +0800

    添加moyu模块

---
+++ module_moyu/src/main/java/com/program/module_moyu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.program.module_moyu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modulemoyu_activity_main);
+    }
+}
