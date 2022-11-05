2d1bc732a6f5dc793c8f2dbaed256a5d5407736e
precontext <815172923@qq.com>
2022-11-05 16:37:31 +0800

    添加收藏集详情的内容跳转

--- module_ucenter/src/main/java/com/program/module_ucenter/ui/FavoriteListActivity.java
+++ module_ucenter/src/main/java/com/program/module_ucenter/ui/FavoriteListActivity.java
@@ -1,17 +1,21 @@
 package com.program.module_ucenter.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.program.lib_base.LogUtils;
 import com.program.lib_common.RoutePath;
 import com.program.module_ucenter.R;
@@ -21,6 +25,7 @@ import com.program.module_ucenter.presenter.IFavoriteListPresenter;
 import com.program.module_ucenter.utils.PresenterManager;
 import com.program.moudle_base.model.CollectionBean;
 import com.program.moudle_base.model.FavoriteBean;
+import com.program.moudle_base.utils.CommonViewUtils;
 import com.program.moudle_base.utils.ToastUtils;
 import com.scwang.smart.refresh.layout.SmartRefreshLayout;
 import com.trello.rxlifecycle4.LifecycleTransformer;
@@ -54,13 +59,22 @@ public class FavoriteListActivity extends AppCompatActivity implements IFavorite
     }
 
     private void initListener() {
-
+        mAdapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(@NonNull BaseQuickAdapter<?, ?> adapter, @NonNull View view, int position) {
+                Object item = adapter.getItem(position);
+                if (item instanceof FavoriteBean.DataBean.ContentBean) {
+                    CommonViewUtils.toWebView(((FavoriteBean.DataBean.ContentBean) item).getUrl());
+                }
+            }
+        });
     }
 
     private void initView() {
         RelativeLayout includerBar = this.findViewById(R.id.include_bar);
         mRefreshLayout = this.findViewById(R.id.refreshLayout);
         TextView tvTitle = includerBar.findViewById(R.id.tv_title);
+        includerBar.findViewById(R.id.ivBack).setOnClickListener(view -> finish());
         if (mCollectionBean != null) {
             tvTitle.setText(mCollectionBean.getName());
             tvTitle.setGravity(Gravity.FILL);
