1d92c6c4c0090c723a2c25dde30fd86011977fb6
precontext <815172923@qq.com>
2022-11-14 16:01:26 +0800

    为动态编辑页添加图片选择

--- module_moyu/src/main/java/com/program/module_moyu/adapter/ImagePreviewAdapter.java
+++ module_moyu/src/main/java/com/program/module_moyu/adapter/ImagePreviewAdapter.java
@@ -6,10 +6,11 @@ import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.program.module_moyu.R;
+import com.program.moudle_base.model.ImageItem;
 
 import org.jetbrains.annotations.NotNull;
 
-public class ImagePreviewAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+public class ImagePreviewAdapter extends BaseQuickAdapter<ImageItem, BaseViewHolder> {
 
     public ImagePreviewAdapter() {
         super(R.layout.modulemoyu_image_choose_item);
@@ -19,14 +20,14 @@ public class ImagePreviewAdapter extends BaseQuickAdapter<String, BaseViewHolder
     }
 
     @Override
-    protected void convert(@NotNull BaseViewHolder viewHolder, String s) {
-        if (s==null||s.equals("")){
+    protected void convert(@NotNull BaseViewHolder viewHolder, ImageItem s) {
+        if (s==null||s.getPath().equals("")){
             return;
         }
         ImageView ivPhoto =viewHolder.getView(R.id.iv_photo);
         ImageView ivClear =viewHolder.getView(R.id.iv_clear);
         Glide.with(ivPhoto.getContext())
-                .load(s)
+                .load(s.getPath())
                 .into(ivPhoto);
         Glide.with(ivClear.getContext())
                 .load(R.mipmap.ic_clear)
--- module_moyu/src/main/java/com/program/module_moyu/ui/activity/PutFishActivity.java
+++ module_moyu/src/main/java/com/program/module_moyu/ui/activity/PutFishActivity.java
@@ -1,29 +1,48 @@
 package com.program.module_moyu.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemChildClickListener;
+import com.hjq.bar.OnTitleBarListener;
+import com.hjq.bar.TitleBar;
+import com.program.lib_base.LogUtils;
 import com.program.lib_common.RoutePath;
+import com.program.lib_common.service.ucenter.wrap.UcenterServiceWrap;
 import com.program.module_moyu.R;
 import com.program.module_moyu.adapter.ImagePreviewAdapter;
+import com.program.moudle_base.model.ImageItem;
+import com.program.moudle_base.utils.ImagePickerConfig;
+import com.program.moudle_base.utils.ToastUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Route(path = RoutePath.Moyu.PAGE_PUT_FISH)
 public class PutFishActivity extends AppCompatActivity {
 
     private static final int  INPUT_MAX_LENGTH = 1024;
+    private static final int  MAX_SELECTED_COUNT = 4;
     private TextView mTvInputLength;
     private EditText mEtInputContent;
     private RecyclerView mRvPreviewImage;
     private ImageView mIvImage;
     private ImagePreviewAdapter mAdapter;
+    private List<ImageItem> mSelectImage = new ArrayList<>();
+    private ImagePickerConfig mPickerConfig;
+    private TitleBar mTitleBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +51,72 @@ public class PutFishActivity extends AppCompatActivity {
         initView();
         initPresenter();
         initEvent();
+        initPickerConfig();
     }
 
     private void initEvent() {
+        mTitleBar.setOnTitleBarListener(new OnTitleBarListener() {
+            @Override
+            public void onLeftClick(TitleBar titleBar) {
+                finish();
+            }
+        });
+        mIvImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mSelectImage.size()<MAX_SELECTED_COUNT){
+                    //控制图片的总数
+                    mPickerConfig.setMaxSelectCount(MAX_SELECTED_COUNT-mSelectImage.size());
+                    UcenterServiceWrap.Singletion.INSTANCE.getHolder().launchImageSwitch(MAX_SELECTED_COUNT-mSelectImage.size());
+                }else {
+                    ToastUtils.showToast("图片只能选"+MAX_SELECTED_COUNT+"张");
+                }
+            }
+        });
+        mAdapter.setOnItemChildClickListener(new OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(@NonNull BaseQuickAdapter adapter, @NonNull View view, int position) {
+                int id = view.getId();
+                Object item = adapter.getItem(position);
+                if (id==R.id.iv_clear){
+                    if (item instanceof ImageItem){
+                        mSelectImage.remove(item);
+                        mAdapter.getData().remove(item);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
+    }
 
+    //得到所选图片
+    private void initPickerConfig() {
+        mPickerConfig = ImagePickerConfig.getInstance();
+        mPickerConfig.setMaxSelectCount(MAX_SELECTED_COUNT);
+        mPickerConfig.setOnImageSelectFinishedListener(new ImagePickerConfig.OnImageSelectFinishedListener() {
+            @Override
+            public void onSelectedFinished(List<ImageItem> result) {
+                for (ImageItem imageItem : result) {
+//                    if (!mSelectImage.contains(imageItem)) {
+//                        mSelectImage.add(imageItem);
+//                    }
+                    boolean isExist = false;
+                    for (ImageItem item : mSelectImage) {
+                        if (item.getPath().equals(imageItem.getPath())){
+                            isExist=true;
+                            ToastUtils.showToast("重复的图片不会添加");
+                            break;
+                        }
+                    }
+                    if (!isExist){
+                        mSelectImage.add(imageItem);
+                    }
+
+                }
+                mAdapter.getData().clear();
+                mAdapter.addData(mSelectImage);
+            }
+        });
     }
 
     private void initPresenter() {
@@ -52,7 +133,7 @@ public class PutFishActivity extends AppCompatActivity {
         mAdapter = new ImagePreviewAdapter();
         mRvPreviewImage.setAdapter(mAdapter);
         mIvImage = this.findViewById(R.id.iv_image);
-
+        mTitleBar = this.findViewById(R.id.title_bar);
     }
 
 }
